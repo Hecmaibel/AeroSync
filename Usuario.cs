62c06e248b309d8d2950e4843168b6306b0e3f90
Jhen <146364737+jheniferDuarte5@users.noreply.github.com>
2025-07-11 09:55:53 -0500

    Add files via upload

---
+++ Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Usuario
+{
+	// ATRIBUTOS 
+
+
+	public int idUsuario { get; set; }
+	public string nombreUsuario { get; set; }
+	public string emailUsuario { get; set; }
+	public string rolUsuario { get; set; }
+	public string nacionalidadUsuario { get; set; }
+	public string passwordUsuario { get; set; }
+
+
+
+    // CONSTRUCTOR 
+
+	public Usuario()
+	{
+
+
+	}
+
+    public Usuario(int idUsuario, string nombreUsuario, string emailUsuario, string rolUsuario, string nacionalidadUsuario, string passwordUsuario)
+    {
+
+		this.idUsuario = idUsuario;
+		this.nombreUsuario	= nombreUsuario;
+		this.emailUsuario = emailUsuario;
+		this.rolUsuario = rolUsuario;
+		this.nacionalidadUsuario = nacionalidadUsuario;
+		this.passwordUsuario = passwordUsuario;
+    }
+
+}
